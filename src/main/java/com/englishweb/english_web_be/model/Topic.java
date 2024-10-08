898270d7520ccffd54fa6cff2ee428a2da534010
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2024-10-08 14:40:39 +0700

    Pagination Topic

---
+++ src/main/java/com/englishweb/english_web_be/model/Topic.java
@@ -0,0 +1,74 @@
+package com.englishweb.english_web_be.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Topic {
+    @Id
+    String id;
+    String title;
+    int serial;
+    String image;
+    String description;
+    StatusEnum status;
+    public Topic() {
+    }
+
+    public Topic(String id, String title, int serial, String image, String description, StatusEnum status) {
+        this.id = id;
+        this.title = title;
+        this.serial = serial;
+        this.image = image;
+        this.description = description;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getSerial() {
+        return serial;
+    }
+
+    public void setSerial(int serial) {
+        this.serial = serial;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/com/englishweb/english_web_be/repository/TopicRepository.java
@@ -0,0 +1,12 @@
+package com.englishweb.english_web_be.repository;
+
+import com.englishweb.english_web_be.model.Topic;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TopicRepository extends JpaRepository<Topic, String> {
+    Page<Topic> findAllTopics(Pageable pageable);
+}
