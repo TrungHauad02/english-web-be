004e167c9819e3d7356b4a1157af19e539ea7bd6
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2024-10-13 21:21:31 +0700

    Model, Repo, DTO, Service ListenAndWriteA Word

---
+++ src/main/java/com/englishweb/english_web_be/dto/ListenAndWriteAWordDTO.java
@@ -0,0 +1,82 @@
+package com.englishweb.english_web_be.dto;
+
+import com.englishweb.english_web_be.modelenum.StatusEnum;
+
+public class ListenAndWriteAWordDTO {
+    String id;
+    int serial;
+    String audioUrl;
+    String sentence;
+    int missingIndex;
+    String correctAnswer;
+    StatusEnum status;
+
+    public ListenAndWriteAWordDTO() {
+    }
+
+    public ListenAndWriteAWordDTO(String id, int serial, String audioUrl, String sentence, int missingIndex, String correctAnswer, StatusEnum status) {
+        this.id = id;
+        this.serial = serial;
+        this.audioUrl = audioUrl;
+        this.sentence = sentence;
+        this.missingIndex = missingIndex;
+        this.correctAnswer = correctAnswer;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getSerial() {
+        return serial;
+    }
+
+    public void setSerial(int serial) {
+        this.serial = serial;
+    }
+
+    public String getAudioUrl() {
+        return audioUrl;
+    }
+
+    public void setAudioUrl(String audioUrl) {
+        this.audioUrl = audioUrl;
+    }
+
+    public String getSentence() {
+        return sentence;
+    }
+
+    public void setSentence(String sentence) {
+        this.sentence = sentence;
+    }
+
+    public int getMissingIndex() {
+        return missingIndex;
+    }
+
+    public void setMissingIndex(int missingIndex) {
+        this.missingIndex = missingIndex;
+    }
+
+    public String getCorrectAnswer() {
+        return correctAnswer;
+    }
+
+    public void setCorrectAnswer(String correctAnswer) {
+        this.correctAnswer = correctAnswer;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+}
