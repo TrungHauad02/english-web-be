fc9b2c23fd045c9ffa976ffb031f51c05549ab18
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2024-10-14 11:21:49 +0700

    Add Grammar Controller

---
+++ src/main/java/com/englishweb/english_web_be/controller/GrammarController.java
@@ -0,0 +1,25 @@
+package com.englishweb.english_web_be.controller;
+
+import com.englishweb.english_web_be.dto.GrammarDTO;
+import com.englishweb.english_web_be.service.GrammarService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class GrammarController {
+    GrammarService service;
+
+    public GrammarController(GrammarService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/grammars")
+    public Page<GrammarDTO> retrieveGrammarsByPage(@RequestParam int page, int size){
+        return service.retrieveGrammarsByPage(page, size, Sort.by("Serial"));
+    }
+}
