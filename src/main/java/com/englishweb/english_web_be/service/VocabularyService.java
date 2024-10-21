b05caadfc6a16f0736053f4792fb853a26f5d239
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2024-10-21 13:11:10 +0700

    delete func findByPage in VocabController

--- src/main/java/com/englishweb/english_web_be/service/VocabularyService.java
+++ src/main/java/com/englishweb/english_web_be/service/VocabularyService.java
@@ -2,7 +2,6 @@ package com.englishweb.english_web_be.service;
 
 import com.englishweb.english_web_be.dto.VocabularyDTO;
 import com.englishweb.english_web_be.model.Vocabulary;
-import com.englishweb.english_web_be.repository.TopicRepository;
 import com.englishweb.english_web_be.repository.VocabularyRepository;
 import com.englishweb.english_web_be.util.ValidationUtils;
 import org.springframework.context.annotation.Lazy;
@@ -22,7 +21,7 @@ public class VocabularyService extends BaseService<Vocabulary, VocabularyDTO, Vo
 
     public Page<VocabularyDTO> findByPageTopicId(int page, int size, String sortBy, String sortDir, Class<VocabularyDTO> dtoClass, String topicId){
         ValidationUtils.getInstance().validatePageRequestParam(page, size, sortBy, dtoClass);
-        ValidationUtils.getInstance().validateExistId(topicService.repository, topicId);
+        topicService.isExist(topicId);
         Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name())
                 ? Sort.by(sortBy).ascending()
                 : Sort.by(sortBy).descending();
