d3533c01cf6b0e1a7160308945a0bc608a2eaa6a
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2024-11-09 15:17:57 +0700

    Fix lỗi import req res

--- src/main/java/com/englishweb/english_web_be/service/TestListeningAnswerService.java
+++ src/main/java/com/englishweb/english_web_be/service/TestListeningAnswerService.java
@@ -1,9 +1,6 @@
 package com.englishweb.english_web_be.service;
 
 import com.englishweb.english_web_be.dto.TestListeningAnswerDTO;
-import com.englishweb.english_web_be.dto.request.TestListeningAnswerRequestDTO;
-import com.englishweb.english_web_be.dto.response.TestListeningAnswerResponseDTO;
-import com.englishweb.english_web_be.model.TestListeningAnswer;
 
 import java.util.List;
 
