85a5ece2643c40c9b151dc1ce8da4ebea513f527
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2024-11-09 14:36:31 +0700

    Xoá req res dto

--- src/main/java/com/englishweb/english_web_be/mapper/BaseMapper.java
+++
@@ -1,7 +0,0 @@
-package com.englishweb.english_web_be.mapper;
-
-public interface BaseMapper <DTO, RequestDTO, ResponseDTO>{
-    DTO mapToDTO(RequestDTO requestDTO);
-
-    ResponseDTO mapToResponseDTO(DTO dto);
-}
