cf40cd20bf7185f7eb285aabba20975d0168bda1
hung <hungat418@gmail.com>
2024-12-02 22:55:14 +0700

    fix bug email khoong co dinh dang html

--- src/main/java/com/englishweb/english_web_be/service/impl/EmailServiceImpl.java
+++ src/main/java/com/englishweb/english_web_be/service/impl/EmailServiceImpl.java
@@ -1,9 +1,11 @@
 package com.englishweb.english_web_be.service.impl;
 
 import com.englishweb.english_web_be.service.EmailService;
-import org.springframework.mail.SimpleMailMessage;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,22 +15,49 @@ public class EmailServiceImpl implements EmailService {
     private JavaMailSender mailSender;
 
     public void sendPasswordByEmail(String toEmail, String password) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("Email send by Admin H2T English <hungat418@gmail.com>");
-        message.setTo(toEmail);
-        message.setSubject("Your Account Password");
-        message.setText("Your account has been created.\n\nHere is your username: " + toEmail + " \nand password: " + password +
-                "\n\nPlease change it after your first login.");
-        mailSender.send(message);
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setFrom("Email send by Admin H2T English <hungat418@gmail.com>");
+            helper.setTo(toEmail);
+            helper.setSubject("Your Account Password");
+
+            String htmlContent = "<html><body>"
+                    + "<h3>Your account has been created.</h3>"
+                    + "<p>Here is your username: <strong>" + toEmail + "</strong></p>"
+                    + "<p>Your password: <strong>" + password + "</strong></p>"
+                    + "<p><em>Please change it after your first login.</em></p>"
+                    + "</body></html>";
+
+            helper.setText(htmlContent, true);
+
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 
     public void sendOtpByEmail(String toEmail, String otp) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("Email send by Admin H2T English <hungat418@gmail.com>");
-        message.setTo(toEmail);
-        message.setSubject("Your OTP Code");
-        message.setText("We have received a request to reset your password.\n\nHere is your OTP code: " + otp +
-                "\n\nPlease use this code within a limited time to reset your password. If you did not request this, please ignore this email.");
-        mailSender.send(message);
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setFrom("Email send by Admin H2T English <hungat418@gmail.com>");
+            helper.setTo(toEmail);
+            helper.setSubject("Your OTP Code");
+
+            String htmlContent = "<html><body>"
+                    + "<h3>We have received a request to reset your password.</h3>"
+                    + "<p>Here is your OTP code: <strong>" + otp + "</strong></p>"
+                    + "<p><em>Please use this code within a limited time to reset your password. If you did not request this, please ignore this email.</em></p>"
+                    + "</body></html>";
+
+            helper.setText(htmlContent, true);
+
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }
