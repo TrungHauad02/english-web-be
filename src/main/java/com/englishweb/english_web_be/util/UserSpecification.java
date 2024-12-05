d313ed06a522f0a55a0c882accb2188d1a82ee2f
hung <hungat418@gmail.com>
2024-12-05 16:31:21 +0700

    sua lai tim kiem theo ngay admin, xoa file ApplicationConfig

--- src/main/java/com/englishweb/english_web_be/util/UserSpecification.java
+++ src/main/java/com/englishweb/english_web_be/util/UserSpecification.java
@@ -3,6 +3,9 @@ import com.englishweb.english_web_be.model.User;
 import com.englishweb.english_web_be.modelenum.LevelEnum;
 import com.englishweb.english_web_be.modelenum.RoleEnum;
 import org.springframework.data.jpa.domain.Specification;
+import jakarta.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 import java.time.LocalDate;
 
 public class UserSpecification {
@@ -22,24 +25,24 @@ public class UserSpecification {
     public static Specification<User> hasDateRange(LocalDate searchStartDate, LocalDate searchEndDate) {
         return (root, query, criteriaBuilder) -> {
             if (searchStartDate == null && searchEndDate == null) {
-                return criteriaBuilder.conjunction(); // Không lọc theo ngày
+                return criteriaBuilder.conjunction();
             }
 
-            if (searchStartDate != null && searchEndDate != null) {
-                // Khi cả startDate và endDate đều có giá trị
-                return criteriaBuilder.and(
-                        criteriaBuilder.greaterThanOrEqualTo(root.get("startDate"), searchStartDate),
-                        criteriaBuilder.lessThanOrEqualTo(root.get("startDate"), searchEndDate)
-                );
-            } else if (searchStartDate != null) {
-                // Khi chỉ có startDate
-                return criteriaBuilder.greaterThanOrEqualTo(root.get("startDate"), searchStartDate);
-            } else {
-                // Khi chỉ có endDate
-                return criteriaBuilder.lessThanOrEqualTo(root.get("startDate"), searchEndDate);
+            // Dùng danh sách các điều kiện để kiểm tra
+            List<Predicate> predicates = new ArrayList<>();
+
+            if (searchStartDate != null) {
+                predicates.add(criteriaBuilder.greaterThanOrEqualTo(root.get("startDate"), searchStartDate));
             }
+
+            if (searchEndDate != null) {
+                predicates.add(criteriaBuilder.lessThanOrEqualTo(root.get("startDate"), searchEndDate));
+            }
+
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
     }
+
 
     public static Specification<User> hasLevel(LevelEnum level) {
         return (root, query, criteriaBuilder) -> {
