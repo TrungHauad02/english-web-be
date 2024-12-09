50b69d12a79491676ec24145aa4705642d197a02
CapTan2003 <113422566+CapTan2003@users.noreply.github.com>
2024-12-09 17:39:47 +0700

    xoá dữ liệu trên firebase khi xoá dữ liệu

--- src/main/java/com/englishweb/english_web_be/service/impl/SubmitTestSpeakingServiceImpl.java
+++ src/main/java/com/englishweb/english_web_be/service/impl/SubmitTestSpeakingServiceImpl.java
@@ -4,34 +4,48 @@ import com.englishweb.english_web_be.dto.SubmitTestSpeakingDTO;
 import com.englishweb.english_web_be.model.SubmitTestSpeaking;
 import com.englishweb.english_web_be.repository.SubmitTestSpeakingRepository;
 import com.englishweb.english_web_be.service.SubmitTestSpeakingService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class SubmitTestSpeakingServiceImpl extends BaseServiceImpl<SubmitTestSpeaking, SubmitTestSpeakingDTO, SubmitTestSpeakingRepository>
         implements SubmitTestSpeakingService {
 
     private final SubmitTestServiceImpl submitTestService;
     private final TestSpeakingQuestionServiceImpl testSpeakingQuestionService;
+    private final FirebaseStorageServiceImpl firebaseStorageService;
 
     public SubmitTestSpeakingServiceImpl(SubmitTestSpeakingRepository repository,
                                          @Lazy SubmitTestServiceImpl submitTestService,
-                                         @Lazy TestSpeakingQuestionServiceImpl testSpeakingQuestionService) {
+                                         @Lazy TestSpeakingQuestionServiceImpl testSpeakingQuestionService, FirebaseStorageServiceImpl firebaseStorageService) {
         super(repository);
         this.submitTestService = submitTestService;
         this.testSpeakingQuestionService = testSpeakingQuestionService;
+        this.firebaseStorageService = firebaseStorageService;
     }
-
     public List<SubmitTestSpeakingDTO> findAllBySubmitTestId(String submitTestId) {
         List<SubmitTestSpeaking> entities = repository.findAllBySubmitTest_Id(submitTestId);
         if (entities == null || entities.isEmpty()) {
             return Collections.emptyList();
         }
         return entities.stream().map(this::convertToDTO).collect(Collectors.toList());
+    }
+    public void delete(String id) {
+        SubmitTestSpeakingDTO dto = super.findById(id);
+        try {
+            firebaseStorageService.deleteFile(dto.getContent());
+
+        } catch (IOException e) {
+            log.error("Error occurred while deleting audio for SubmitTestSpeaking with ID: {}", dto.getId(), e);
+        }
+        super.delete(id);
     }
 
     @Override
--- src/main/java/com/englishweb/english_web_be/service/impl/TestListeningServiceImpl.java
+++ src/main/java/com/englishweb/english_web_be/service/impl/TestListeningServiceImpl.java
@@ -1,28 +1,40 @@
 package com.englishweb.english_web_be.service.impl;
 
+
 import com.englishweb.english_web_be.dto.TestListeningDTO;
 import com.englishweb.english_web_be.dto.TestListeningQuestionDTO;
-import com.englishweb.english_web_be.dto.TestReadingQuestionDTO;
+
 import com.englishweb.english_web_be.model.TestListening;
 import com.englishweb.english_web_be.model.TestListeningQuestion;
 import com.englishweb.english_web_be.modelenum.StatusEnum;
 import com.englishweb.english_web_be.repository.TestListeningRepository;
 import com.englishweb.english_web_be.service.TestListeningService;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
-import java.util.Comparator;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
 @Service
+@Slf4j
 public class TestListeningServiceImpl extends BaseServiceImpl<TestListening, TestListeningDTO, TestListeningRepository> implements TestListeningService {
 
     private final TestServiceImpl testService;
     @Getter
     private final TestListeningQuestionServiceImpl testListeningQuestionService;
+    private final FirebaseStorageServiceImpl firebaseStorageService;
+
+
+    public TestListeningServiceImpl(TestListeningRepository repository, @Lazy TestServiceImpl testService, @Lazy TestListeningQuestionServiceImpl testListeningQuestionService, FirebaseStorageServiceImpl firebaseStorageService) {
+        super(repository);
+        this.testService = testService;
+        this.testListeningQuestionService = testListeningQuestionService;
+        this.firebaseStorageService = firebaseStorageService;
+    }
 
     public List<TestListeningDTO> findAllByTestId(String test_id) {
         testService.isExist(test_id);
@@ -56,13 +68,6 @@ public class TestListeningServiceImpl extends BaseServiceImpl<TestListening, Tes
     }
 
 
-    public TestListeningServiceImpl(TestListeningRepository repository, @Lazy TestServiceImpl testService, @Lazy TestListeningQuestionServiceImpl testListeningQuestionService) {
-        super(repository);
-        this.testService = testService;
-        this.testListeningQuestionService = testListeningQuestionService;
-    }
-
-
 
     public int totalActiveListeningQuestionsByTestId(String testId) {
         testService.isExist(testId);
@@ -86,9 +91,6 @@ public class TestListeningServiceImpl extends BaseServiceImpl<TestListening, Tes
                 })
                 .sum();
     }
-
-
-
 
     @Override
     protected TestListening convertToEntity(TestListeningDTO dto) {
@@ -116,7 +118,13 @@ public class TestListeningServiceImpl extends BaseServiceImpl<TestListening, Tes
     }
     @Override
     public void delete(String id) {
+        TestListeningDTO dto = super.findById(id);
+        try {
+            firebaseStorageService.deleteFile(dto.getContent());
 
+        } catch (IOException e) {
+            log.error("Error occurred while deleting audio for TestListening with ID: {}", dto.getId(), e);
+        }
         List<TestListeningQuestionDTO> questions = testListeningQuestionService.findAllByTestListening_Id(id);
         if (questions != null) {
             for (TestListeningQuestionDTO question : questions) {
@@ -124,20 +132,6 @@ public class TestListeningServiceImpl extends BaseServiceImpl<TestListening, Tes
             }
         }
         super.delete(id);
-    }
-
-    public void deleteQuestionTest(String questionId) {
-
-        testListeningQuestionService.delete(questionId);
-    }
-
-    public void updateQuestionTest(List<TestListeningQuestionDTO> questions) {
-
-        if (questions != null) {
-            for (TestListeningQuestionDTO question : questions) {
-                testListeningQuestionService.update(question,question.getId());
-            }
-        }
     }
 
 }
