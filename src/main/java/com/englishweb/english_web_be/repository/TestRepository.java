fc493fc044ce5a16411e3f2934f5f2572f5d06bb
CapTan2003 <113422566+CapTan2003@users.noreply.github.com>
2024-12-12 08:27:23 +0700

    cập nhật lại serial các bài kiểm tra khi xoá một bài kiểm tra

--- src/main/java/com/englishweb/english_web_be/repository/TestRepository.java
+++ src/main/java/com/englishweb/english_web_be/repository/TestRepository.java
@@ -3,18 +3,21 @@ package com.englishweb.english_web_be.repository;
 import com.englishweb.english_web_be.model.Test;
 import com.englishweb.english_web_be.modelenum.StatusEnum;
 import com.englishweb.english_web_be.modelenum.TestTypeEnum;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TestRepository extends JpaRepository<Test, String>  , JpaSpecificationExecutor<Test> {
 
 
     Test findByIdAndStatus(String id, StatusEnum status);
+    @NotNull List<Test> findAll();
     @Query("SELECT MAX(t.serial) FROM Test t")
     Integer findMaxSerial();
 
